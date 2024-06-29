9c41b5d3b4a1b36511e796d9bc19406f9bfa8195
saiharshav <vsharsha48@gmail.com>
2024-06-29 23:11:38 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+})
+
+app.listen("3000", () => {
+  console.log("Server running on port 3000");
+})
